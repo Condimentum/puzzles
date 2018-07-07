0132de0e2bef2eebefa2faf8756001cb866d898f
Axel Johansson <axel.j.johansson@gmail.com>
2018-07-07 17:14:06 +0300

    Implement solutions. Unit test succesful.

--- src/main/java/Puzzles.java
+++ src/main/java/Puzzles.java
@@ -8,11 +8,22 @@ public class Puzzles
      * Select the longest words from the input list. That is, select the words
      * whose lengths are equal to the maximum word length.
      */
+    // Time complexity: O(n)
     public static List<String> selectLongestWords( List<String> input )
     {
-        //TODO: add your code here
         List<String> result = new ArrayList<>();
-
+        int maxLen = 0;
+        for (String word : input) {
+            // If word is longer than any word before, set maxLen and clear result
+            if (word.length() > maxLen) {
+                maxLen = word.length();
+                result = new ArrayList<>();
+            }
+            // If word is as long as the longest word so far, add it to list
+            if (word.length() == maxLen) {
+                result.add(word);
+            }
+        }
 
         return result;
     }
@@ -22,11 +33,16 @@ public class Puzzles
      * Given a list of words, create an output list that contains
      * only the odd-length words, converted to upper case.
      */
+    // Time complexity: O(n)
     public static List<String> upcaseOddLengthWords( List<String> input)
     {
-        //TODO: Add your code here
         List<String> result = new ArrayList<>();
-
+        for (String word : input) {
+            // If the word length is odd, add uppercase version to list
+            if (word.length() % 2 == 1) {
+                result.add(word.toUpperCase());
+            }
+        }
 
         return result;
     }
@@ -38,11 +54,22 @@ public class Puzzles
      * initial character. When concatenating the words, they should be
      * separated by a colon (':').
      */
+    // Time complexity: O(n)
     public static Map<Character, String> mapOfStringByInitialCharacter( List<String> input )
     {
-
-        //TODO: Add your code here
         Map<Character, String> result = new TreeMap<>();
+        for (String word : input) {
+            // Get lower case inital character
+            Character initial = Character.toLowerCase(word.charAt(0));
+            // If map key already exists, add word to the value
+            if(result.containsKey(initial)) {
+                result.put(initial, result.get(initial) + ":" + word);
+            }
+            // If map key doesn't exist, assign the word to the key
+            else {
+                result.put(initial, word);
+            }
+        }
 
         return result;
     }
@@ -73,8 +100,14 @@ public class Puzzles
      */
     public static List<String> denormilizeMap( Map<Integer, List<String>> input )
     {
-        //TODO:: Add your code here
         List<String> result = new ArrayList<>();
+        // Iterate (key, value) pairs
+        input.forEach((Integer k, List<String> v) -> {
+            // Iterate animal list and add animal to result list
+            for (String animal : v) {
+                result.add(animal + ":" + k.toString());
+            }
+        });
 
         return result;
     }
@@ -109,8 +142,23 @@ public class Puzzles
      */
     public static Map<Integer, Set<String>> invertMultiMap( Map<String, Set<Integer>> input )
     {
-        //TODO add your code here
-        Map<Integer, Set<String>> result = null;
+        Map<Integer, Set<String>> result = new TreeMap<>();
+        input.forEach((k, v) -> {
+            for (int number : v) {
+                Set<String> newValue;
+                // If map key exists, get current value set
+                if(result.containsKey(number)) {
+                    newValue = result.get(number);
+                }
+                // If map key doesn't exist, create new value set
+                else {
+                    newValue = new TreeSet<>();
+                }
+                // Add key to value set
+                newValue.add(k);
+                result.put(number, newValue);
+            }
+        });
 
         return result;
     }
